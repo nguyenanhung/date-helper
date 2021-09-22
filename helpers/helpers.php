40aec8ebef78e526e5405860ce00f9d4fe61045a
nguyenanhung <info@nguyenanhung.com>
2021-09-22 21:05:09 +0700

    init helper

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -7,3 +7,17 @@
  * Date: 09/22/2021
  * Time: 19:11
  */
+if (!function_exists('iso_8601_utc_time')) {
+    /**
+     * Function iso_8601_utc_time
+     *
+     * @return string|null
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 08/18/2021 57:13
+     */
+    function iso_8601_utc_time()
+    {
+        return nguyenanhung\Libraries\DateAndTime\DateAndTime::zuluTime();
+    }
+}
